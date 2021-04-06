826bd80a8a169e6bde0d62f727520e828ed73599
Calin Capitanu <calincapitanu@gmail.com>
2021-04-07 00:54:15 +0200

    consent cookie

---
+++ cookie-banner.js
@@ -0,0 +1,37 @@
+const cookieStorage = {
+	getItem: (key) => {
+		const cookies = document.cookie
+			  .split(';')
+			  .map(cookie => cookie.split('='))
+			  .reduce((acc, [key, value]) => ({...acc, [key.trim()]: value}), {});
+		return cookies[key];
+		
+	},
+	setItem: (key, value) => {
+		document.cookie = `${key}=${value}`;
+	},
+};
+
+
+const storageType = cookieStorage;
+const consentPropertyName = 'sib_consent';
+
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () => {
+	const acceptFn = event => {
+        saveToStorage(storageType);
+        consentPopup.classList.add('hidden');
+     };
+    const consentPopup = document.getElementById('consent-popup');
+    const acceptBtn = document.getElementById('accept');
+    acceptBtn.addEventListener('click', acceptFn);
+
+	
+	if (shouldShowPopup(storageType)) {
+        setTimeout(() => {
+            consentPopup.classList.remove('hidden');
+        }, 2000);
+    }
+};
