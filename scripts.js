0d045beadd54624d5c8b3ed4cb8beb3e02864a57
Calin Capitanu <calincapitanu@gmail.com>
2021-04-18 00:30:21 +0200

    wip

--- scripts.js
+++ scripts.js
@@ -1,19 +1,27 @@
 function change_text(pic){
 	if(pic == 1){
 		document.getElementById("cont-title").innerHTML = "Work together!";
-		document.getElementById("cont-desc").innerHTML = "Work with students from different backgrounds and fields of work to come up with a solution to your challenge";
+		document.getElementById("cont-desc").innerHTML = "Work with students from different backgrounds and fields <br/> of work to come up with a solution to your challenge";
+		document.getElementById("cont-button").innerHTML = "Find out more";
+		document.getElementById("cont-button").onclick = "window.location.href='about-us.html#about-the-bootcamp";
 	}
 	if(pic == 2){
 		document.getElementById("cont-title").innerHTML = "Learn from the pro's!";
-		document.getElementById("cont-desc").innerHTML = "Our mentors will guide you during the challenge and provide important insights in innovation and business strategy";
+		document.getElementById("cont-desc").innerHTML = "Our mentors will guide you during the challenge and provide  <br/> important insights in innovation and business strategy";
+		document.getElementById("cont-button").innerHTML = "I want to become a mentor!";
+		document.getElementById("cont-button").onclick = "window.location.href='wip.html'";
 	}
 	if(pic == 3){
 		document.getElementById("cont-title").innerHTML = "Real problems, real solutions";
-		document.getElementById("cont-desc").innerHTML = "Our previous bootcamp resulted in solutions and cooperations that kept going even after the event";
+		document.getElementById("cont-desc").innerHTML = "Our previous bootcamp resulted in solutions and cooperations <br/> that kept going even after the event";
+		document.getElementById("cont-button").innerHTML = "Become a partner";
+		document.getElementById("cont-button").onclick = "window.location.href='wip.html'";
 	}
 	if(pic == 4){
 		document.getElementById("cont-title").innerHTML = "Meet your new CO-founder";
-		document.getElementById("cont-desc").innerHTML = "The bootcamp is a great place to meet people from different diciplines and who knows, start the next unicorn together with";
+		document.getElementById("cont-desc").innerHTML = "The bootcamp is a great place to meet people from  <br/> different diciplines and who knows, start the next <br/> unicorn together with";
+		document.getElementById("cont-button").innerHTML = "Sign up";
+		document.getElementById("cont-button").onclick = "window.location.href='wip.html'";
 	}
 	
 }
